6ae21147bb700c01f8cd7d6cbbece8e11658a2bf
Mohd uvais wali khan <uvaiswalikhan@gmail.com>
2025-10-03 02:38:39 +0530

    Delete village360-frontend directory

--- village360-frontend/src/components/auth/auth-provider.tsx
+++
@@ -1,5 +0,0 @@
-import { AuthProvider as BaseAuthProvider } from "@/hooks/use-auth";
-
-export function AuthProvider({ children }: { children: React.ReactNode }) {
-  return <BaseAuthProvider>{children}</BaseAuthProvider>;
-}
